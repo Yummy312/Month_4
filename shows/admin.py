4d3f54566fd8222c980935e51be0039f7f7e9daf
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2022-04-20 22:32:56 +0300

    add app and changes in the settings.py else urls

---
+++ shows/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.BookShow)
+admin.site.register(models.BookFeedBack)
---
+++ shows/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from . import models
+
+
+def shows_all(request):
+    shows = models.BookShow.objects.all()
+    return render(request, "shows_list.html", {"shows": shows})
+
+
+def show_detail(request, id):
+    show = get_object_or_404(models.BookShow, id=id)
+    return render(request, "shows_detail.html", {"show": show})
+
