fcdda027c7a8217b15ed8c96305d24d4e8ef63b4
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2022-04-28 01:43:25 +0300

    The done

--- book/urls.py
+++ book/urls.py
@@ -3,9 +3,16 @@ from . import views
 app_name = 'book'
 urlpatterns = [
     path('books/', views.book_all, name='book_all'),
+    path('catalog/fantastic/', views.show_genre_fantastic, name="catalog_fantastic"),
+    path('catalog/romantic/', views.show_genre_romantic, name="catalog_romantic"),
+    path('catalog/horror/', views.show_genre_horror, name="catalog_horror"),
+    path('catalog/latest/', views.latest_date, name="catalog_latest"),
     path('catalog/', views.book_show, name="book_show"),
     path('catalog/<int:id>/', views.show_detail, name="show_detail"),
-    path('add-book/', views.add_book, name='add_book')
+    path('catalog/<int:id>/update/', views.update_book, name='catalog_update'),
+    path('catalog/<int:id>/delete/', views.book_delete, name='catalog_delete'),
+    path('add-book/', views.add_book, name='add_book'),
+
 
 ]
 
--- book/views.py
+++ book/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from . import models, forms
+from datetime import datetime, timedelta
+
+date = datetime.today() - timedelta(days=2)
 
 
 def book_all(request):
@@ -8,7 +11,7 @@ def book_all(request):
 
 
 def book_show(request):
-    show_book = models.BookShow.objects.all()
+    show_book = models.BookShow.objects.filter().order_by("-id")
     return render(request, "show_books.html", {"show_book": show_book})
 
 
@@ -28,3 +31,48 @@ def add_book(request):
         form = forms.BookForm()
     return render(request, "add_books.html", {'form': form})
 
+
+def book_delete(request, id):
+    book_id = get_object_or_404(models.BookShow, id=id)
+    book_id.delete()
+    return redirect(reverse("book:book_show"))
+
+
+def update_book(request, id):
+    book_id = get_object_or_404(models.BookShow, id=id)
+    if request.method == 'POST':
+        form = forms.BookForm(instance=book_id,
+                              data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('book:book_show'))
+    else:
+        form = forms.BookForm(instance=book_id)
+    return render(request, "book_update.html", {"form": form,
+                                                "detail": book_id})
+
+
+def latest_date(request):
+    show_book = models.BookShow.objects.filter(created_date__gt=date).order_by("-id")
+    return render(request, "show_books.html", {"show_book": show_book})
+
+
+def show_genre_horror(request):
+    show_book = models.BookShow.objects.filter(genre="Horror")
+    return render(request, "show_books.html", {"show_book": show_book})
+
+
+def show_genre_romantic(request):
+    show_book = models.BookShow.objects.filter(genre="Romantic")
+    return render(request, "show_books.html", {"show_book": show_book})
+
+
+def show_genre_fantastic(request):
+    show_book = models.BookShow.objects.filter(genre="Fantastic")
+    return render(request, "show_books.html", {"show_book": show_book})
+
+
+def book_delete_(request, id):
+    book_id = get_object_or_404(models.BookShow, id=id)
+    book_id.delete()
+    return redirect(reverse("book:book_show"))
