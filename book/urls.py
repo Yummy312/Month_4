ad6dccaa9b4b546dc2e88b367b3dd3cd0c3f89ca
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2022-04-27 15:50:59 +0300

    My misses

--- book/urls.py
+++ book/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-
+app_name = 'book'
 urlpatterns = [
     path('books/', views.book_all, name='book_all'),
     path('catalog/', views.book_show, name="book_show"),
     path('catalog/<int:id>/', views.show_detail, name="show_detail"),
+    path('add-book/', views.add_book, name='add_book')
 
 ]
 
--- book/views.py
+++ book/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from . import models
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from . import models, forms
 
 
 def book_all(request):
@@ -15,3 +15,16 @@ def book_show(request):
 def show_detail(request, id):
     detail = get_object_or_404(models.BookShow, id=id)
     return render(request, "details.html", {"detail": detail})
+
+
+def add_book(request):
+    method = request.method
+    if method == 'POST':
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('book:book_show'))
+    else:
+        form = forms.BookForm()
+    return render(request, "add_books.html", {'form': form})
+
