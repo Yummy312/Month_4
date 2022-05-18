209426342e5ee50a8522ca16078fd4a6c4196476
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2022-05-18 21:12:11 +0300

    test

--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,76 @@
 from django.test import TestCase
+from . import models
 
-# Create your tests here.
+
+class TestModel(TestCase):
+
+    def test_model_BookShow_create_success(self):
+        book = {
+            "title": "Test title",
+            "author": "Test author",
+            "description": "Test description",
+            "image": "Test image",
+            "created_date": "Test created_date",
+            "update_date": "Test update_date",
+            "genre": "Test genre",
+            "year_of_issue": 122
+
+        }
+        content = models.BookShow.objects.create(**book)
+        self.assertEqual(content.author, book['author'])
+
+    def test_model_BookShow_create_fail(self):
+        book = {
+            "title": "Test title",
+            "author": True,
+            "description": "Test description",
+            "image": "Test image",
+            "created_date": "Test created_date",
+            "update_date": "Test update_date",
+            "genre": "Test genre",
+            "year_of_issue": 122,
+            "page": "Test Page"
+
+        }
+
+        with self.assertRaises(TypeError):
+            content = models.BookShow.objects.create(**book)
+
+    def test_model_BookShow__update(self):
+        book = {
+            "title": "Test title",
+            "author": "Test author",
+            "description": "Test description",
+            "image": "Test image",
+            "created_date": "Test created_date",
+            "update_date": "Test update_date",
+            "genre": "Test genre",
+            "year_of_issue": 122
+
+        }
+
+        new_title = "TITLE"
+        content = models.BookShow.objects.create(**book)
+        content.title = new_title
+        content.save()
+        content.refresh_from_db()
+        print(content.title)
+
+    def test_model_BookShow__delete(self):
+        book = {
+            "title": "Test title",
+            "author": "Test author",
+            "description": "Test description",
+            "image": "Test image",
+            "created_date": "Test created_date",
+            "update_date": "Test update_date",
+            "genre": "Test genre",
+            "year_of_issue": 122
+
+        }
+        content = models.BookShow.objects.create(**book)
+        pk = content.pk
+        print(pk)
+        content.delete()
+        with self.assertRaises(models.BookShow.DoesNotExist):
+            models.BookShow.objects.get(pk=pk)
--- sketchy/tests.py
+++ sketchy/tests.py
@@ -1,3 +1,64 @@
 from django.test import TestCase
+from unittest import TestCase
+from . import models, parser
 
-# Create your tests here.
+
+class TestModel(TestCase):
+
+    def test_model_Content_create_success(self):
+        temp = {
+            "title": "Test title",
+            "image": "Test image",
+            "url": " Test url"
+        }
+        content = models.Content.objects.create(**temp)
+        self.assertEqual(content.title, temp['title'])
+
+    def test_model_Content__create_fail(self):
+        temp = {
+            "title": "Test title",
+            "image": "Test image",
+            "url": 122
+        }
+
+        with self.assertRaises(ValueError):
+            content = models.Content.objects.create(**temp)
+            raise ValueError("You need to enter the other values.")
+
+    def test_model_Content__update(self):
+        temp = {
+            "title": "Test title",
+            "image": "Test image",
+            "url": " Test url"
+        }
+        new_url = "URL"
+        content = models.Content.objects.create(**temp)
+        content.url = new_url
+        content.save()
+        content.refresh_from_db()
+        print(content.url)
+
+    def test_model_Content__delete(self):
+        temp = {
+            "title": "Test title",
+            "image": "Test image",
+            "url": " Test url"
+        }
+        content = models.Content.objects.create(**temp)
+        pk = content.pk
+        print(pk)
+        content.delete()
+        with self.assertRaises(models.Content.DoesNotExist):
+            models.Content.objects.get(pk=pk)
+
+
+class TestParser(TestCase):
+    def test_parser_function(self):
+        with self.assertRaises(TypeError):
+            data = parser.parser_func(-1)
+            raise TypeError('parser_func() takes 0 positional arguments but 1 was given')
+        with self.assertRaises(AttributeError):
+            data = parser.get_htm()
+            raise AttributeError("The module has no attribute")
+
+
